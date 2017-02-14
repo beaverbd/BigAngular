5a6d5b87bdffe1b3fe22ad1193b31bc4116fc57b
DESKTOP-3OFKQOG\developer\BosakDmytro <d.bosak@newline.tech>
2017-02-14 16:26:41 +0200

    added files for customer module

---
+++ app/customer/history/history.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'history',
+	templateUrl: './history/history.component.html',
+	styleUrls: ['./history/history.component.css']
+})
+
+export class HistoryComponent implements OnInit {
+
+	constructor() { }
+
+	ngOnInit() { }
+}
