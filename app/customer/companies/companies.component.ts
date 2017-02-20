ae5684eec48c80029521d9d1502daa890b9db603
Dmytro Bosak nout <bosak.dmitrii@gmail.com>
2017-02-21 01:06:58 +0200

    fixed some issues with global.component

--- app/customer/companies/companies.component.ts
+++ app/customer/companies/companies.component.ts
@@ -6,7 +6,6 @@ import { Injector} from '@angular/core';
 import { Company } from '../../entity/company';
 import { Product } from '../../entity/product';
 import { AccountService } from '../../services/account.service';
-import { GlobalComponent } from '../../global.component';
 import { FinanceService } from '../../services/finance.service';
 
 @Component({
@@ -15,7 +14,7 @@ import { FinanceService } from '../../services/finance.service';
 	styleUrls: ['companies.component.css']
 })
 
-export class CompaniesComponent extends GlobalComponent  implements OnInit {
+export class CompaniesComponent implements OnInit {
 
 	companies:Company[];
 	selectedCompany:Company;
@@ -24,8 +23,8 @@ export class CompaniesComponent extends GlobalComponent  implements OnInit {
 	count:number = 0;
 	inProgress:boolean=false;
 	result:string;
-	constructor(private dataPeekerService:DataPeekerService,private financeservice:FinanceService, injector:Injector) { 
-		super(injector);
+	constructor(private dataPeekerService:DataPeekerService,private financeservice:FinanceService,
+	private accountService:AccountService) { 
 		this.dataPeekerService.getCompanies()
 		.subscribe(result=>{
 			console.log("!!!");
